3cd63b546965820e86cadee196447e4603bd16a6
Maria Malenkova <maria.malenkova@gmail.com>
2023-04-22 20:24:35 -0500

    Playgrounds list styling

--- src/components/PlaygroundsList.js
+++ src/components/PlaygroundsList.js
@@ -1,14 +1,26 @@
 const PlaygroundsList = ({ playgrounds = [], currentPage, pageSize }) => {
     return (
         <>
-            <h3>Playgrounds:</h3>
             <ul>
                 {playgrounds.map((place, i) => (
-                    <li key={place.id}>
-                        <p className="text-green-600">
-                            {(currentPage - 1) * pageSize + i + 1}. {place.name}
-                        </p>
-                        <p>{place.address}</p>
+                    <li
+                        key={place.id}
+                        className="flex border border-gray-200 rounded-md mb-3 px-3 py-2 shadow-sm 
+                        hover:cursor-pointer
+                        hover:border-gray-400"
+                    >
+                        {place.image ? (
+                            <img src="{place.image}" alt={place.name}></img>
+                        ) : (
+                            <div className="bg-gray-200 w-28 h-20"></div>
+                        )}
+                        <div className="ml-3">
+                            <p className="text-green-600 font-bold">
+                                {(currentPage - 1) * pageSize + i + 1}.{" "}
+                                {place.name}
+                            </p>
+                            <p>{place.address}</p>
+                        </div>
                     </li>
                 ))}
             </ul>
