a5fdcf5ece40c6dda0a8d465c9257cf745a16899
Maria Malenkova <maria.malenkova@gmail.com>
2023-05-17 15:50:41 -0500

    Move NavBar to component folder

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from "./NavBar";
+import NavBar from "./components/NavBar";
 import HomePage from "./pages/HomePage";
 import AboutPage from "./pages/AboutPage";
 import ContactsPage from "./pages/ContactsPage";
