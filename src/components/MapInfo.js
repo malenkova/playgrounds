f111c76f04b940a55bf26682c79b9a3ed7697938
Maria Malenkova <maria.malenkova@gmail.com>
2023-06-06 10:28:05 -0500

    Redirect from the map info to place page when click

--- src/components/MapInfo.js
+++ src/components/MapInfo.js
@@ -1,7 +1,11 @@
+import { Link } from "react-router-dom";
+
 const MapInfo = ({ place }) => {
     return (
         <div className="w-40">
-            <h3 className="font-semibold">{place.name}</h3>
+            <Link to={`/playgrounds/${place.id}`}>
+                <h3 className="font-semibold text-my-green">{place.name}</h3>
+            </Link>
             <p>{place.address}</p>
         </div>
     );
